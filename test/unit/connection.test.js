4066e852c88cb4a7cbe5871e3d5a2e6c5a8e16fd
delvedor <tommydelved@gmail.com>
2019-07-04 12:17:42 +0200

    Fix test

--- test/unit/connection.test.js
+++ test/unit/connection.test.js
@@ -761,7 +761,8 @@ test('Util.inspect Connection class should hide agent and ssl', t => {
   resurrectTimeout: 0,
   _openRequests: 0,
   status: 'alive',
-  roles: { master: true, data: true, ingest: true, ml: false } }`)
+  roles: { master: true, data: true, ingest: true, ml: false },
+  weight: 0 }`)
   )
 })
 
@@ -803,7 +804,7 @@ test('Should not add agent and ssl to the serialized connection', t => {
 
   t.strictEqual(
     JSON.stringify(connection),
-    '{"url":"http://localhost:9200/","id":"http://localhost:9200/","headers":null,"deadCount":0,"resurrectTimeout":0,"_openRequests":0,"status":"alive","roles":{"master":true,"data":true,"ingest":true,"ml":false}}'
+    '{"url":"http://localhost:9200/","id":"http://localhost:9200/","headers":null,"deadCount":0,"resurrectTimeout":0,"_openRequests":0,"status":"alive","roles":{"master":true,"data":true,"ingest":true,"ml":false},"weight":0}'
   )
 
   t.end()
